23a3f5f2c807958e00754f12604ec679dd3283aa
Tare Ebelo <75279482+TareHimself@users.noreply.github.com>
2024-01-18 15:57:26 -0500

    vma namespace change

--- cmake/VulkanMemoryAllocator-HppConfig.cmake
+++ cmake/VulkanMemoryAllocator-HppConfig.cmake
@@ -1,6 +1,6 @@
 include (CMakeFindDependencyMacro)
 
 find_dependency (Vulkan REQUIRED)
-find_dependency (VulkanMemoryAllocator REQUIRED)
+# find_dependency (VulkanMemoryAllocator REQUIRED) #Why is this needed ? The one file "vk_mem_alloc.h" is copied to the install dir
 
 include (${CMAKE_CURRENT_LIST_DIR}/VulkanMemoryAllocator-HppTargets.cmake)
